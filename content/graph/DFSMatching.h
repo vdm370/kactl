8976d73ce480562dbe691c4eb7e3fc645e9df366
KacperWalentynowicz <kwalentynowicz12@gmail.com>
2021-09-08 09:47:39 +0100

    Improved matchings. Added Dinic algorithm to list of things we want in the library.

--- content/graph/DFSMatching.h
+++ content/graph/DFSMatching.h
@@ -1,38 +1,39 @@
 /**
- * Author: Lukas Polacek
- * Date: 2009-10-28
+ * Author: Kacper Walentynowicz
+ * Date: 2021-09-07
  * License: CC0
  * Source:
- * Description: Simple bipartite matching algorithm. Graph $g$ should be a list
- * of neighbors of the left partition, and $btoa$ should be a vector full of
- * -1's of the same size as the right partition. Returns the size of
- * the matching. $btoa[i]$ will be the match for vertex $i$ on the right side,
- * or $-1$ if it's not matched.
- * Time: O(VE)
- * Usage: vi btoa(m, -1); dfsMatching(g, btoa);
- * Status: works
+ * Description: Simple bipartite matching algorithm. Graph $g$ should be given as normal adjacency list, no need to specify partitions explicitly
+ * Time: $O(VE)$ worst case but also for $n,m \leq 10^5$ in 0.5s
+ * Status: tested on SPOJ and LibraryChecker
  */
 #pragma once
 
-bool find(int j, vector<vi>& g, vi& btoa, vi& vis) {
-	if (btoa[j] == -1) return 1;
-	vis[j] = 1; int di = btoa[j];
-	for (int e : g[di])
-		if (!vis[e] && find(e, g, btoa, vis)) {
-			btoa[e] = di;
-			return 1;
-		}
-	return 0;
-}
-int dfsMatching(vector<vi>& g, vi& btoa) {
-	vi vis;
-	rep(i,0,sz(g)) {
-		vis.assign(sz(btoa), 0);
-		for (int j : g[i])
-			if (find(j, g, btoa, vis)) {
-				btoa[j] = i;
-				break;
-			}
-	}
-	return sz(btoa) - (int)count(all(btoa), -1);
-}
+struct matching {
+  vector<vi> v;
+  int n;
+  vi odw, skoj;
+  matching(vector <vi> &vec) : v(vec), n(sz(vec)), odw(n, 0), skoj(n, -1) {}
+  bool dfs(int x) {
+	  if (odw[x]) return 0;
+	  odw[x] = 1;
+	  trav(u, v[x]) {
+		  if (skoj[u] == -1 || dfs(skoj[u])) {
+			  skoj[u] = x;
+			  skoj[x] = u;
+			  return 1;
+		  }
+	  }
+	  return 0;
+  }
+  int solve() {
+    int ok = 1, res = 0;
+    while (ok--) {
+      fill(all(odw), 0);
+      rep(i, 0, n) {
+		  if (skoj[i] == -1 && dfs(i)) res++, ok = 1;
+	  }
+    }
+    return res;
+  }
+};
