459482d9c6a98eb968dcf3cf9bd6b88d2593d16c
KacperWalentynowicz <kwalentynowicz12@gmail.com>
2021-09-08 12:52:58 +0100

    Added Lazy Segment Tree

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -1,66 +1,82 @@
 /**
- * Author: Simon Lindholm
+ * Author: AtCoder Library + Kacper Walentynowicz
  * Date: 2016-10-08
  * License: CC0
  * Source: me
- * Description: Segment tree with ability to add or set values of large intervals, and compute max of intervals.
- * Can be changed to other things.
- * Use with a bump allocator for better performance, and SmallPtr or implicit indices to save memory.
+ * Description: Segment tree with good lazy propagation templates. Queries [, ). You give two monoids and one acts on the other by monoid homomorphisms.
  * Time: O(\log N).
- * Usage: Node* tr = new Node(v, 0, sz(v));
- * Status: stress-tested a bit
+ * Usage: Couting bitwise inversions on ranges:
+struct S {mint a; int size;};
+struct F {mint a, b;};
+S op(S l, S r) { return S{l.a + r.a, l.size + r.size}; }
+S e() { return S{0, 0}; }
+S mapping(F l, S r) { return S{r.a * l.a + r.size * l.b, r.size}; }
+F composition(F l, F r) { return F{r.a * l.a, r.b * l.a + l.b}; }
+F id() { return F{1, 0}; } 
+
+lazy_segtree<S, op, e, F, mapping, composition, id> seg(vec);
+ * Status: tested on atcoder problems
  */
 #pragma once
-
-#include "../various/BumpAllocator.h"
-
-const int inf = 1e9;
-struct Node {
-	Node *l = 0, *r = 0;
-	int lo, hi, mset = inf, madd = 0, val = -inf;
-	Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of -inf
-	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
-		if (lo + 1 < hi) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
-			val = max(l->val, r->val);
-		}
-		else val = v[lo];
-	}
-	int query(int L, int R) {
-		if (R <= lo || hi <= L) return -inf;
-		if (L <= lo && hi <= R) return val;
-		push();
-		return max(l->query(L, R), r->query(L, R));
-	}
-	void set(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) mset = val = x, madd = 0;
-		else {
-			push(), l->set(L, R, x), r->set(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void add(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) {
-			if (mset != inf) mset += x;
-			else madd += x;
-			val += x;
-		}
-		else {
-			push(), l->add(L, R, x), r->add(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void push() {
-		if (!l) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(lo, mid); r = new Node(mid, hi);
-		}
-		if (mset != inf)
-			l->set(lo,hi,mset), r->set(lo,hi,mset), mset = inf;
-		else if (madd)
-			l->add(lo,hi,madd), r->add(lo,hi,madd), madd = 0;
-	}
+template <class S,S (*op)(S, S), S (*e)(),
+          class F, S (*mapping)(F, S), F (*composition)(F, F),F (*id)()>
+struct lazy_segtree {
+private:
+    int n, size, log;
+    vector<S> d; vector<F> lz;
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+    void all_apply(int k, F f) {
+        d[k] = mapping(f, d[k]);
+        if (k < size) lz[k] = composition(f, lz[k]);
+    }
+    void push(int k) {
+        all_apply(2 * k, lz[k]);
+        all_apply(2 * k + 1, lz[k]);
+        lz[k] = id();
+    }
+    void make_pushes(int &l, int &r) {
+        l += size; r += size;
+        for (int i = log; i >= 1; i--) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push(r >> i);
+        }
+    }
+public:
+    lazy_segtree(const vector<S>& v) : n(int(v.size())) {
+        log = 32 - __builtin_clz(n); size = 1 << log;
+        d = vector<S>(2 * size, e());
+        lz = vector<F>(size, id());
+        rep(i, 0, n) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) update(i);
+    }
+    void set(int p, S x) {
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        d[p] = x;
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+    S prod(int l, int r) {
+        if (l >= r) return e();
+        make_pushes(l, r);
+        S sml = e(), smr = e();
+        for (; l < r; l >>= 1, r >>= 1) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+        }
+        return op(sml, smr);
+    }
+    void apply(int l, int r, F f) {
+        if (l >= r) return;
+        make_pushes(l, r);
+        int initl = l, initr = r;
+        for (; l < r; l >>= 1, r >>= 1) {
+            if (l & 1) all_apply(l++, f);
+            if (r & 1) all_apply(--r, f);
+        }
+        l = initl; r = initr;
+        for (int i = 1; i <= log; i++) {
+            if (((l >> i) << i) != l) update(l >> i);
+            if (((r >> i) << i) != r) update((r - 1) >> i);
+        }
+    }
 };
