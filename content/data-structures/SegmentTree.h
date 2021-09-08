41c12c4240ccbc1dc9882b0bd47b9d48185654c2
KacperWalentynowicz <kwalentynowicz12@gmail.com>
2021-09-08 10:56:21 +0100

    Atcoder's simple segtree.

--- content/data-structures/SegmentTree.h
+++ content/data-structures/SegmentTree.h
@@ -1,30 +1,37 @@
 /**
- * Author: Lucian Bicsi
- * Date: 2017-10-31
+ * Author: AtCoder library + Kacper Walentynowicz
+ * Date: 2021-09-08
  * License: CC0
  * Source: folklore
- * Description: Zero-indexed max-tree. Bounds are inclusive to the left and exclusive to the right. Can be changed by modifying T, f and unit.
- * Time: O(\log N)
+ * Description: Zero-indexed segtree. Queries [,). 
+ * Usage: segtree<int, op, e> seg(vec);
  * Status: stress-tested
  */
 #pragma once
 
-struct Tree {
-	typedef int T;
-	static constexpr T unit = INT_MIN;
-	T f(T a, T b) { return max(a, b); } // (any associative fn)
-	vector<T> s; int n;
-	Tree(int n = 0, T def = unit) : s(2*n, def), n(n) {}
-	void update(int pos, T val) {
-		for (s[pos += n] = val; pos /= 2;)
-			s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
-	}
-	T query(int b, int e) { // query [b, e)
-		T ra = unit, rb = unit;
-		for (b += n, e += n; b < e; b /= 2, e /= 2) {
-			if (b % 2) ra = f(ra, s[b++]);
-			if (e % 2) rb = f(s[--e], rb);
-		}
-		return f(ra, rb);
-	}
+template <class S, S (*op)(S, S), S (*e)()> struct segtree {
+  private:
+	int n, size;
+	vector<S> d;
+	void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+  public:
+    segtree(const vector<S>& v) : n(sz(v)) {
+		size = 1 << (32 - __builtin_clz(n));
+        d = vector<S>(2 * size, e());
+        rep(i, 0, n) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) update(i);
+    }
+    void set(int pos, S val) {
+        for (d[pos += n] = val; pos /= 2;)
+			d[pos] = op(d[pos * 2], d[pos * 2 + 1]);
+    }
+    S get(int p) {return d[p + size];}
+    S prod(int l, int r) {
+        S sml = e(), smr = e();
+        for (l += size, r += size; l < r; l >>= 1, r >>= 1) {
+			if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+        }
+        return op(sml, smr);
+    }
 };
