4d1a28b62b5bb37d05b901bf5eab61c267aabc85
KacperWalentynowicz <kwalentynowicz12@gmail.com>
2021-09-14 23:37:15 +0100

    Final update on HLD. Improving docs, removing the need for root=0, some simplifications.

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -17,37 +17,37 @@ F id() { return F{1, 0}; }
 lazy_segtree<S, op, e, F, mapping, composition, id> seg(vec);
  * Status: tested on atcoder problems
  */
-template <class S,S (*op)(S, S), S (*e)(),
-          class F, S (*mapping)(F, S), F (*composition)(F, F),F (*id)()>
+template <class S, S(*op)(S, S), S(*e)(),
+class F, S (*mapping)(F, S), F(*composition)(F, F), F (*id)()>
 struct lazy_segtree {
 private:
-    int n, size, log;
-    vector<S> d; vector<F> lz;
-    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
-    void all_apply(int k, F f) {
-        d[k] = mapping(f, d[k]);
-        if (k < size) lz[k] = composition(f, lz[k]);
-    }
-    void push(int k) {
-        all_apply(2 * k, lz[k]);
-        all_apply(2 * k + 1, lz[k]);
-        lz[k] = id();
-    }
-    void make_pushes(int &l, int &r) {
-        l += size; r += size;
-        for (int i = log; i >= 1; i--) {
-            if (((l >> i) << i) != l) push(l >> i);
-            if (((r >> i) << i) != r) push(r >> i);
-        }
-    }
+	int n, size, log;
+	vector <S> d; vector <F> lz;
+	void update(int k) {d[k] = op(d[k << 1], d[k << 1 | 1]); }
+	void all_apply(int k, F f) {
+		d[k] = mapping(f, d[k]);
+		if (k < size) lz[k] = composition(f, lz[k]);
+	}
+	void push(int k) {
+		all_apply(k << 1, lz[k]);
+		all_apply(k << 1 | 1, lz[k]);
+		lz[k] = id();
+	}
+	void make_pushes(int &l, int &r) {
+		l += size; r += size;
+		for (int i = log; i > 0; --i) {
+			if (((l >> i) << i) != l) push(l >> i);
+			if (((r >> i) << i) != r) push(r >> i);
+		}
+	}
 public:
-    lazy_segtree(const vector<S>& v) : n(int(v.size())) {
-        log = 32 - __builtin_clz(n); size = 1 << log;
-        d = vector<S>(2 * size, e());
-        lz = vector<F>(size, id());
-        rep(i, 0, n) d[size + i] = v[i];
-        for (int i = size - 1; i >= 1; i--) update(i);
-    }
+	lazy_segtree(const vector <S> &v) : n(sz(v)) {
+		log = 32 - __builtin_clz(n); size = 1 << log;
+		d = vector<S>(2*size, e());
+		lz = vector<F>(size, id());
+		rep(i, 0, n) d[size + i] = v[i];
+		for (int i=size-1; i>0; --i) update(i);
+	}
     void set(int p, S x) {
         p += size;
         for (int i = log; i >= 1; i--) push(p >> i);
@@ -55,27 +55,27 @@ public:
         for (int i = 1; i <= log; i++) update(p >> i);
     }
     S prod(int l, int r) {
-        if (l >= r) return e();
-        make_pushes(l, r);
-        S sml = e(), smr = e();
-        for (; l < r; l >>= 1, r >>= 1) {
-            if (l & 1) sml = op(sml, d[l++]);
-            if (r & 1) smr = op(d[--r], smr);
-        }
-        return op(sml, smr);
-    }
-    void apply(int l, int r, F f) {
-        if (l >= r) return;
-        make_pushes(l, r);
-        int initl = l, initr = r;
-        for (; l < r; l >>= 1, r >>= 1) {
-            if (l & 1) all_apply(l++, f);
-            if (r & 1) all_apply(--r, f);
-        }
-        l = initl; r = initr;
-        for (int i = 1; i <= log; i++) {
-            if (((l >> i) << i) != l) update(l >> i);
-            if (((r >> i) << i) != r) update((r - 1) >> i);
-        }
-    }
+		if (l >= r) return e();
+		make_pushes(l, r);
+		S sml = e(), smr = e();
+		for (; l < r; l >>= 1, r >>= 1) {
+			if (l & 1) sml = op(sml, d[l++]);
+			if (r & 1) smr = op(d[--r], smr);
+		}
+		return op(sml, smr);
+	}
+	void apply(int l, int r, F f) {
+		if (l >= r) return;
+		make_pushes(l, r);
+		int initl = l, initr = r;
+		for (; l < r; l >>= 1, r >>= 1) {
+			if (l & 1) all_apply(l++, f);
+			if (r & 1) all_apply(--r, f);
+		}
+		l = initl; r = initr;
+		rep(i, 1, log+1) {
+			if (((l >> i) << i) != l) update(l >> i);
+			if (((r >> i) << i) != r) update((r - 1) >> i);
+		}
+	}
 };
--- content/graph/HLD.h
+++ content/graph/HLD.h
@@ -2,31 +2,24 @@
  * Author: Benjamin Qi, Oleksandr Kulkov, chilli, kpw29
  * Date: 2020-01-12
  * License: CC0
- * Source: https://codeforces.com/blog/entry/53170, https://github.com/bqi343/USACO/blob/master/Implementations/content/graphs%20(12)/Trees%20(10)/HLD%20(10.3).h
- * Description: Decomposes a tree into vertex disjoint heavy paths and light
- * edges such that the path from any leaf to the root contains at most log(n)
- * light edges. Code does additive modifications and max queries, but can
- * support commutative segtree modifications/queries on paths and subtrees.
- * Takes as input the full adjacency list. VALS\_EDGES being true means that
- * values are stored in the edges, as opposed to the nodes. All values
- * initialized to the segtree default. Root must be 0.
- * Time: O((\log N)^2)
+ * Description: Handles subtree and path queries simultaneously in one lazy\_segtree. Each subtree is $1$ segment, while path is $O(\log N)$ segments in the tree. $VALS\_EDGES$ being true means that values are stored in the edges, as opposed to the nodes. All values are initialized to the segtree default.
+ * Time: O((\log N)^2), one logarithm for subtrees.
  * Status: non-exhaustive testing on library-checker website.
  */
 template <bool VALS_EDGES,
-class S, S (*op)(S, S), S (*e)(),
-class F, S (*mapping)(F, S), F (*composition)(F, F),F (*id)()>
+class S, S(*op)(S, S), S(*e)(),
+class F, S (*mapping)(F, S), F(*composition)(F, F), F (*id)()>
 struct HLD {
 	int N, tim = 0;
-	vector<vi> adj;
+	vector <vi> adj;
 	vi par, siz, depth, rt, pos;
-	lazy_segtree <S, op, e, F, mapping, composition, id> tree;
-	HLD(vector<vi> adj_)
-		: N(sz(adj_)), adj(adj_), par(N, -1), siz(N, 1), depth(N),
-		  rt(N),pos(N),tree(vector<S>(N, e())) { dfsSz(0); dfsHld(0); }
+	lazy_segtree<S, op, e, F, mapping, composition, id> tree;
+	HLD(vector <vi> adj_, int root=0) : 
+		N(sz(adj_)), adj(adj_), par(N, -1), siz(N, 1), depth(N),
+		rt(N,root), pos(N), tree(vector<S>(N, e())) {dfsSz(root), dfsHld(root);}
 	void dfsSz(int v) {
 		if (par[v] != -1) adj[v].erase(find(all(adj[v]), par[v]));
-		for (int& u : adj[v]) {
+		trav(u, adj[v]){
 			par[u] = v, depth[u] = depth[v] + 1;
 			dfsSz(u);
 			siz[v] += siz[u];
@@ -35,12 +28,12 @@ struct HLD {
 	}
 	void dfsHld(int v) {
 		pos[v] = tim++;
-		for (int u : adj[v]) {
+		trav(u, adj[v]){
 			rt[u] = (u == adj[v][0] ? rt[v] : u);
 			dfsHld(u);
 		}
 	}
-	template <class B> void process(int u, int v, B query) {
+	template<class B> void process(int u, int v, B query) {
 		for (; rt[u] != rt[v]; v = par[rt[v]]) {
 			if (depth[rt[u]] > depth[rt[v]]) swap(u, v);
 			query(pos[rt[v]], pos[v] + 1);
@@ -49,7 +42,7 @@ struct HLD {
 		query(pos[u] + VALS_EDGES, pos[v] + 1);
 	}
 	void path_apply(int u, int v, F func) {
-		process(u, v, [&](int l, int r) { tree.apply(l, r, func); });
+		process(u, v, [&](int l, int r) {tree.apply(l, r, func); });
 	}
 	S path_prod(int u, int v) {
 		S res = e();
@@ -57,11 +50,5 @@ struct HLD {
 			res = op(res, tree.prod(l, r));
 		});
 		return res;
-	}
-	void subtree_apply(int v, F func) {
-		tree.apply(pos[v] + VALS_EDGES, pos[v] + siz[v], func);
-	}
-	S subtree_prod(int v) {
-		return tree.prod(pos[v] + VALS_EDGES, pos[v] + siz[v]);
 	}
 };
