bca6b31cc60163c5c8be02d048acd1c9869bc166
KacperWalentynowicz <kwalentynowicz12@gmail.com>
2021-09-27 21:19:46 +0100

    Improved docs for MapComparator.h

--- content/data-structures/MapComparator.h
+++ content/data-structures/MapComparator.h
@@ -1,8 +1,9 @@
 /**
  * Author: Benjamin Qi
- * Description: example of function object (functor) for map or set
+ * Description: example of function object (functor); keeps default behavior
+ * cmp(a, a) should always return FALSE(!)
  * Source: StackOverflow
  * Usage: set<int,cmp> s; map<int,int,cmp> m;
  */
 
-struct cmp{bool operator()(int l,int r)const{return l>r;}};
+struct cmp{bool operator()(int l,int r)const{return l<r;}};
