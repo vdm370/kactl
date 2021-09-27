37bfc53f460a954b3b7c6d6b796e987411a89ed1
Maja Trela <maja.ada0@gmail.com>
2021-09-27 23:34:28 +0200

    Debug macro in template

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -8,6 +8,14 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
+template<class A,class B>auto&operator<<(ostream&o,pair<A,B>p){return o<<'('<<p.first<<", "<<p.second<<')';}
+template<class T>auto operator<<(ostream&o,T x)->decltype(x.end(),o){o<<'{';int i=0;for(auto e:x)o<<(", ")+2*!i++<<e;return o<<'}';}
+#ifdef DEBUG
+#define debug(x...) cerr<<"["#x"]: ",[](auto...$){((cerr<<$<<"; "),...);}(x),cerr<<'\n'
+#else
+#define debug(...) {}
+#endif
+
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
